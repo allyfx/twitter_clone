a8aa5f58ec29183f1cd96c8f2015e20335e1a7a5
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-08 21:47:05 -0300

    Added feature: Register User

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+
+	namespace App\Controllers;
+
+	use MF\Controller\Action;
+	use MF\Model\Container;
+
+	class IndexController extends Action {
+
+		public function index() {
+
+			$this->render('index');
+		}
+
+		public function inscreverse() {
+			$this->view->erroCadastro = false;
+			$this->render('inscreverse');
+		}
+
+		public function registrar() {
+			$usuario = Container::getModel('Usuario');
+
+			$usuario->__set('nome', $_POST['nome']);
+			$usuario->__set('email', $_POST['email']);
+			$usuario->__set('senha', $_POST['senha']);
+
+			if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
+				$usuario->salvar();
+
+				$this->render('cadastro');
+			} else {
+				$this->view->erroCadastro = true;
+				
+				$this->render('inscreverse');
+			}
+		}
+
+	}
+
+
+?>
